2e2e7b7b13dad5169e7af53f690e68d53bee7186
Pengyi Wang <pengyi.wang@rbslearn.net>
2025-07-23 20:46:32 +1000

    skill.delete

--- skill.py
+++
@@ -1,7 +0,0 @@
-class Skill:
-    def __init__(self, name, description, level = 1, effect = None):
-        self.name = name
-        self.description = description
-        self.level = level
-        self.effect = effect
-        
