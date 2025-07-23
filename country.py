3af23b51248b04cd643ef4d641a6d7967c7ca092
w07-py <pengyi.wang@rbslearn.net>
2025-07-23 11:47:23 +1000

    Update country.py

--- country.py
+++ country.py
@@ -1,8 +1,9 @@
+from item import Item
 class Country:
     def __init__(self, country_name):
-        self.name = country_name
+        self.country_name = country_name
         self.description = None
-        self.linked_rooms = {}
+        self.linked_countries = {}
         self.character = None
 
     def set_description(self, country_description):
@@ -15,10 +16,10 @@ class Country:
         print(self.description)
 
     def set_name(self, country_name):
-        self.name = country_name
+        self.country_name = country_name
 
     def get_name(self):
-        return self.name
+        return self.country_name
 
     def set_character(self, new_character):
         self.character = new_character
@@ -26,19 +27,42 @@ class Country:
     def get_character(self):
         return self.character 
 
-    def link_room(self, room_to_link, direction):
-        self.linked_rooms[direction] = room_to_link
+    def link_country(self, country_to_link, direction):
+        self.linked_countries[direction] = country_to_link
 
     def get_details(self):
-        for direction in self.linked_rooms:
-            room = self.linked_rooms[direction]
-            print("The " + room.get_name() + " is " + direction)
+        print(self.country_name)
+        print("-------------")
+        print(self.description)
+        for direction in self.linked_countries:
+            country = self.linked_countries[direction]
+            print("The " + country.get_name() + " is " + direction)
 
     def move(self, direction):
-        if direction in self.linked_rooms:
-            return self.linked_rooms[direction]
+        if direction in self.linked_countries:
+            return self.linked_countries[direction]
         else:
             print("You can't go that way")
             return self
 
+class Store:
+    def __init__(self):
+        self.store_items = []
+        self.prices = {}
 
+    def add_item(self, item: Item, price: int = 30):
+        self.store_items.append(item)
+        self.prices[item.name] = price
+
+    def display_store(self):
+        print("\n=== WELCONE TO THE STORE ===")
+        print("Here are the items available for purchase:")
+        print("-" * 50)
+        for idx, item in enumerate(self.store_items, 1):
+            price = self.prices.get(item.name, 30)
+            print(f"{idx}. {item.name} - {price} gold")
+            print(f"{item.description}")
+            print(f"Effect: {item.effect} | Weight: {item.weight}")
+            print()
+        print("Enter item number to buy, or 0 to cancel.")
+        print("-" * 50)
